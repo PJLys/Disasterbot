faf7b0b92ced9d93b0833752e58ed5030e4561e9
jeoffreycanters <92429971+jeoffreycanters@users.noreply.github.com>
2024-01-22 14:26:15 +0100

    Update main.c

--- IoT-Ex1/Core/Src/main.c
+++ IoT-Ex1/Core/Src/main.c
@@ -31,8 +31,7 @@
 uint8_t Rx_data[3];
 uint8_t BT_data[8];
 
-I2C_HandleTypeDef hi2c1; // Handles i2c communication with SHT40
-I2C_HandleTypeDef hi2c3; // Handles i2c communication with LTR329
+I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
@@ -101,24 +100,23 @@ void SHT40_Read(float *t, float *rh, uint8_t mode)
     *rh = rh_pRH;
 }
 
-void LTR329_Init()
-{
+void LTR329_Init() {
     uint8_t data;
 
     // Activate the sensor
     data = LTR329_ACTIVE_MODE;
-    HAL_I2C_Mem_Write(&hi2c3, LTR329_I2C_ADDRESS, LTR329_ALS_CONTR, I2C_MEMADD_SIZE_8BIT, &data, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, LTR329_I2C_ADDRESS, LTR329_ALS_CONTR, I2C_MEMADD_SIZE_8BIT, &data, 1, 100);
 
     // Set measurement rate
     data = LTR329_MEAS_RATE;
-    HAL_I2C_Mem_Write(&hi2c3, LTR329_I2C_ADDRESS, LTR329_ALS_MEAS_RATE, I2C_MEMADD_SIZE_8BIT, &data, 1, 100);
+    HAL_I2C_Mem_Write(&hi2c1, LTR329_I2C_ADDRESS, LTR329_ALS_MEAS_RATE, I2C_MEMADD_SIZE_8BIT, &data, 1, 100);
 }
-void LTR329_Read(uint16_t *ch0, uint16_t *ch1)
-{
+
+void LTR329_Read_Light(uint16_t *ch0, uint16_t *ch1) {
     uint8_t data[4];
 
     // Read 4 bytes of data starting from LTR329_ALS_DATA_CH1_0
-    HAL_I2C_Mem_Read(&hi2c3, LTR329_I2C_ADDRESS, LTR329_ALS_DATA_CH1_0, I2C_MEMADD_SIZE_8BIT, data, 4, 100);
+    HAL_I2C_Mem_Read(&hi2c1, LTR329_I2C_ADDRESS, LTR329_ALS_DATA_CH1_0, I2C_MEMADD_SIZE_8BIT, data, 4, 100);
 
     // Combine bytes to get the light data for each channel
     *ch1 = (uint16_t)(data[1] << 8) | data[0];
@@ -211,7 +209,7 @@ int main(void)
 
 			  	  printf("Waking up from low-power mode...\n");
 		  		  // Perform measurements
-			  	  LTR329_Read(&light_ch0, &light_ch1);
+			  	  LTR329_Read_Light(&light_ch0, &light_ch1);
 		  		  SHT40_Read(&t, &rh, SHT40_MEAS_HIGH_PRECISION);
 		  		  float radiation = t*rh;
 		  		  float light0 = (float)light_ch0;
